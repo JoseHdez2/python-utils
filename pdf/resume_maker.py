6744e664586e81602c974a5b4ebf6450089b6e96
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-11-30 19:30:24 +0000

    init pdf resume maker

---
+++ pdf/resume_maker.py
@@ -0,0 +1,9 @@
+import json
+
+with open('../assets/resume.json') as json_file:
+    data = json.load(json_file)
+    for p in data['people']:
+        print('Name: ' + p['name'])
+        print('Website: ' + p['website'])
+        print('From: ' + p['from'])
+        print('')
